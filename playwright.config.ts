4e3e4dc971be44320094b6e8160cd52307a653b5
mickmister <6913320+mickmister@users.noreply.github.com>
2023-11-16 22:16:24 -0500

    works with todo plugin in parent dir

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import path from 'path';
+import dotenv from 'dotenv';
+import testConfig from '@e2e-test.playwright-config';
+dotenv.config({ path: `${__dirname}/.env` });
+
+// Configuration override for plugin tests
+// testConfig.testDir = __dirname + "/tests";
+// testConfig.outputDir = __dirname + "/test-results";
+
+testConfig.testDir = path.join(__dirname, '../tests');
+testConfig.outputDir = path.join(__dirname, '../test-results');
+
+testConfig.testMatch = "test.list.ts";
+testConfig.timeout = 10 * 1000;
+if (!testConfig.use) {
+  testConfig.use = {};
+}
+testConfig.use.video = {
+  mode: "on",
+  size: { width: 1024, height: 768 },
+};
+testConfig.projects = [
+  {
+    name: "setup",
+    testMatch: /integrations\.setup\.ts/,
+  },
+  {
+    name: "chrome",
+    use: {
+      browserName: "chromium",
+      permissions: ["notifications"],
+      viewport: { width: 1280, height: 1024 },
+      storageState: __dirname + "/.auth-user.json",
+    },
+    dependencies: ["setup"],
+  },
+];
+
+export default testConfig;
