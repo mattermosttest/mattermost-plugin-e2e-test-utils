08db48767e616beb114b797e703ca08fae4c6d81
panoramix360 <lucasreis3000@gmail.com>
2024-02-22 11:38:00 -0300

    feat: configuring the node-fetch package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import fetch from 'node-fetch'
+global.fetch = fetch
+
 export {default as MattermostContainer} from './mmcontainer'
 export {default as RunContainer} from './plugincontainer'
 export * from './utils'
